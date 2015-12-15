5364e787a5e36160d1b099848bd9a27a5e6e7c64
dmilos <dmilos@example.com>
2015-12-15 20:47:23 +0100

    * fix: broken msvc

--- src/color/rgb/convert/yiq.hpp
+++ src/color/rgb/convert/yiq.hpp
@@ -19,15 +19,15 @@ namespace color
           typedef ::color::_internal::trait<category_left_type>    category_left_trait_type;
           typedef ::color::_internal::trait<category_left_type>   category_right_trait_type;
 
-         typedef typename category_left_trait_type::index_type          index_type;
-
-         typedef typename category_left_trait_type::container_trait_type     container_left_trait_type, clt_type;
-         typedef typename category_right_trait_type::container_trait_type    container_right_trait_type, crt_type;
-
-         typedef typename category_left_trait_type::component_type  component_left_type;
-
-         typedef typename container_left_trait_type::input_type         container_left_input_type;
-         typedef typename container_right_trait_type::input_const_type  container_right_const_input_type;
+          typedef  category_left_trait_type::index_type          index_type;
+          
+          typedef  category_left_trait_type::container_trait_type     container_left_trait_type, clt_type;
+          typedef  category_right_trait_type::container_trait_type    container_right_trait_type, crt_type;
+          
+          typedef  category_left_trait_type::component_type  component_left_type;
+          
+          typedef  container_left_trait_type::input_type         container_left_input_type;
+          typedef  container_right_trait_type::input_const_type  container_right_const_input_type;
 
          static void process
           (
