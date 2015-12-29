8fb789651915a24821af7a7829f360bac6810928
dmilos <dmilos@example.com>
2015-12-29 23:23:05 +0100

    * improvement: [doc] more material in documentation * new: [API] rgb_uint24 and class rgb_uint48, just to reserve names

--- src/color/rgb/category.hpp
+++ src/color/rgb/category.hpp
@@ -10,7 +10,9 @@ namespace color
 
     class rgb_uint8    {};
     class rgb_uint16   {};
+    class rgb_uint24   {};
     class rgb_uint32   {};
+    class rgb_uint48   {};
     class rgb_uint64   {};
     class rgb_float    {};
     class rgb_double   {};
