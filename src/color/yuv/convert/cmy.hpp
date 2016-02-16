54cce0b540ae1cee776994684b563e7c4345dbde
dmilos <dmilos@example.com>
2016-02-16 16:06:01 +0100

    * enhance:[internal] conversion from CMY to YUV

--- src/color/yuv/convert/cmy.hpp
+++ src/color/yuv/convert/cmy.hpp
@@ -58,9 +58,9 @@ namespace color
           static scalar_type const Umax = yuv_const_type::Umax();
           static scalar_type const Vmax = yuv_const_type::Vmax();
 
-          scalar_type r = scalar_type(1) - normalize_type::template process<0>( container_right_trait_type::template get<0>( right ) );
-          scalar_type g = scalar_type(1) - normalize_type::template process<1>( container_right_trait_type::template get<1>( right ) );
-          scalar_type b = scalar_type(1) - normalize_type::template process<2>( container_right_trait_type::template get<2>( right ) );
+          scalar_type r = scalar_type(1) - normalize_type::template process<cyan_p   >( container_right_trait_type::template get<cyan_p   >( right ) );
+          scalar_type g = scalar_type(1) - normalize_type::template process<yellow_p >( container_right_trait_type::template get<yellow_p >( right ) );
+          scalar_type b = scalar_type(1) - normalize_type::template process<magenta_p>( container_right_trait_type::template get<magenta_p>( right ) );
 
           scalar_type y = Wr * r + Wg * g + Wb * b;
           //scalar_type u = Umax * ( b - y )/( 1- Wb );
