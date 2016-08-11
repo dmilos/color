79a0071c526190a4b1f896c5612e2dd414daec9f
dejan <dmilos@gmail.com>
2016-08-11 13:50:30 +0200

    * enhance:[internal] type reduction in YCgCo

--- src/color/YCgCo/YCgCo.hpp
+++ src/color/YCgCo/YCgCo.hpp
@@ -13,27 +13,9 @@
 
 namespace color
  {
-  namespace _internal
-   {
-
-    template< typename type_name >
-     struct pick_YCgCo
-      {
-       typedef ::color::category::YCgCo_uint32 category_type;
-      };
-
-
-    template<> struct pick_YCgCo< std::uint8_t   >{ typedef ::color::category::YCgCo_uint8   category_type; };
-    template<> struct pick_YCgCo< std::uint16_t  >{ typedef ::color::category::YCgCo_uint16  category_type; };
-    template<> struct pick_YCgCo< std::uint32_t  >{ typedef ::color::category::YCgCo_uint32  category_type; };
-    template<> struct pick_YCgCo< std::uint64_t  >{ typedef ::color::category::YCgCo_uint64  category_type; };
-    template<> struct pick_YCgCo< float          >{ typedef ::color::category::YCgCo_float   category_type; };
-    template<> struct pick_YCgCo< double         >{ typedef ::color::category::YCgCo_double  category_type; };
-    template<> struct pick_YCgCo< long    double >{ typedef ::color::category::YCgCo_ldouble category_type; };
-   }
 
   template< typename type_name >
-   using YCgCo = ::color::model< typename ::color::_internal::pick_YCgCo< type_name >::category_type >;
+   using YCgCo = ::color::model< typename ::color::category::YCgCo< type_name > >;
 
  }
 
--- src/color/YCgCo/category.hpp
+++ src/color/YCgCo/category.hpp
@@ -6,46 +6,21 @@ namespace color
   namespace category
    {
 
-    namespace _internal
-     {
-
-      template< typename tag_name >
-       struct YCgCo__type
-        {
-         typedef tag_name tag_type;
-        };
-
-      using YCgCo_void    = color::category::_internal::YCgCo__type< void >;
-      using YCgCo_bool    = color::category::_internal::YCgCo__type< bool >;
-      using YCgCo_uint8   = color::category::_internal::YCgCo__type< std::uint8_t >;
-      using YCgCo_uint16  = color::category::_internal::YCgCo__type< std::uint16_t >;
-    //using YCgCo_uint24  = color::category::_internal::YCgCo__type< std::uint24_t >;
-      using YCgCo_uint32  = color::category::_internal::YCgCo__type< std::uint32_t >;
-    //using YCgCo_uint48  = color::category::_internal::YCgCo__type< std::uint48_t >;
-      using YCgCo_uint64  = color::category::_internal::YCgCo__type< std::uint64_t >;
-      using YCgCo_float   = color::category::_internal::YCgCo__type< float >;
-      using YCgCo_double  = color::category::_internal::YCgCo__type< double >;
-      using YCgCo_ldouble = color::category::_internal::YCgCo__type< long double >;
-
-     }
-
     template< typename tag_name >
      struct YCgCo
       {
        typedef void category_name;
       };
 
-    using YCgCo_void    = ::color::category::YCgCo< ::color::category::_internal::YCgCo_void    >;
-    using YCgCo_bool    = ::color::category::YCgCo< ::color::category::_internal::YCgCo_bool    >;
-    using YCgCo_uint8   = ::color::category::YCgCo< ::color::category::_internal::YCgCo_uint8   >;
-    using YCgCo_uint16  = ::color::category::YCgCo< ::color::category::_internal::YCgCo_uint16  >;
-  //using YCgCo_uint24  = ::color::category::YCgCo< ::color::category::_internal::YCgCo_uint24  >;
-    using YCgCo_uint32  = ::color::category::YCgCo< ::color::category::_internal::YCgCo_uint32  >;
-  //using YCgCo_uint48  = ::color::category::YCgCo< ::color::category::_internal::YCgCo_uint48  >;
-    using YCgCo_uint64  = ::color::category::YCgCo< ::color::category::_internal::YCgCo_uint64  >;
-    using YCgCo_float   = ::color::category::YCgCo< ::color::category::_internal::YCgCo_float   >;
-    using YCgCo_double  = ::color::category::YCgCo< ::color::category::_internal::YCgCo_double  >;
-    using YCgCo_ldouble = ::color::category::YCgCo< ::color::category::_internal::YCgCo_ldouble >;
+    using YCgCo_uint8   = ::color::category::YCgCo< std::uint8_t  >;
+    using YCgCo_uint16  = ::color::category::YCgCo< std::uint16_t >;
+  //using YCgCo_uint24  = ::color::category::YCgCo< uint24_t >;
+    using YCgCo_uint32  = ::color::category::YCgCo< std::uint32_t >;
+  //using YCgCo_uint48  = ::color::category::YCgCo< uint48_t >;
+    using YCgCo_uint64  = ::color::category::YCgCo< std::uint64_t >;
+    using YCgCo_float   = ::color::category::YCgCo< float         >;
+    using YCgCo_double  = ::color::category::YCgCo< double        >;
+    using YCgCo_ldouble = ::color::category::YCgCo< long double   >;
 
    }
  }
