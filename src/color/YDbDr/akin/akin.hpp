006b3bda06681a6df7077608be54423ae9be8004
dmilos <dmilos@gmail.com>
2016-08-23 14:04:34 +0200

    * new:[API] Macros to exclude unneeded models at request.

--- src/color/YDbDr/akin/akin.hpp
+++ src/color/YDbDr/akin/akin.hpp
@@ -1,8 +1,51 @@
-#include "./cmy.hpp"
-#include "./cmyk.hpp"
+
+
+
+#ifndef COLOR_EXCLUDE_MODEL_GRAY
 #include "./gray.hpp"
+#endif
+
+#ifndef COLOR_EXCLUDE_MODEL_CMY
+#include "./cmy.hpp"
+#endif
+
+#ifndef COLOR_EXCLUDE_MODEL_CMYK
+#include "./cmyk.hpp"
+#endif
+
+#ifndef COLOR_EXCLUDE_MODEL_HSL
 #include "./hsl.hpp"
+#endif
+
+#ifndef COLOR_EXCLUDE_MODEL_HSV
 #include "./hsv.hpp"
+#endif
+
+#ifndef COLOR_EXCLUDE_MODEL_HSI
+#include "./hsi.hpp"
+#endif
+
+#ifndef COLOR_EXCLUDE_MODEL_RGB
 #include "./rgb.hpp"
+#endif
+
+#ifndef COLOR_EXCLUDE_MODEL_YIQ
 #include "./yiq.hpp"
+#endif
+
+#ifndef COLOR_EXCLUDE_MODEL_YUV
+// TODO #include "./yuv.hpp"
+#endif
+
+#ifndef COLOR_EXCLUDE_MODEL_YCgCo
+// TODO #include "./YCgCo.hpp"
+#endif
+
+#ifndef COLOR_EXCLUDE_MODEL_YDbDr
 #include "./YDbDr.hpp"
+#endif
+
+#ifndef COLOR_EXCLUDE_MODEL_YPbPr
+// TODO #include "./YPbPr.hpp"
+#endif
+
--- src/color/color.body.hpp
+++ src/color/color.body.hpp
@@ -1,21 +1,54 @@
 
 #include "./generic/generic.hpp"
 
-#include "./cmy/cmy.hpp"
-#include "./cmyk/cmyk.hpp"
+#ifndef COLOR_EXCLUDE_MODEL_GRAY
 #include "./gray/gray.hpp"
+#endif
 
+#ifndef COLOR_EXCLUDE_MODEL_CMY
+#include "./cmy/cmy.hpp"
+#endif
+
+#ifndef COLOR_EXCLUDE_MODEL_CMYK
+#include "./cmyk/cmyk.hpp"
+#endif
+
+#ifndef COLOR_EXCLUDE_MODEL_HSL
 #include "./hsl/hsl.hpp"
+#endif
+
+#ifndef COLOR_EXCLUDE_MODEL_HSV
 #include "./hsv/hsv.hpp"
+#endif
+
+#ifndef COLOR_EXCLUDE_MODEL_HSI
 #include "./hsi/hsi.hpp"
+#endif
 
+#ifndef COLOR_EXCLUDE_MODEL_RGB
 #include "./rgb/rgb.hpp"
-#include "./yiq/yiq.hpp"
-#include "./yuv/yuv.hpp"
+#endif
 
-#include "./YPbPr/YPbPr.hpp"
+#ifndef COLOR_EXCLUDE_MODEL_YIQ
+#include "./yiq/yiq.hpp"
+#endif
+
+#ifndef COLOR_EXCLUDE_MODEL_YUV
+#include "./yuv/yuv.hpp"
+#endif
+
+#ifndef COLOR_EXCLUDE_MODEL_YCgCo
 #include "./YCgCo/YCgCo.hpp"
+#endif
+
+#ifndef COLOR_EXCLUDE_MODEL_YDbDr
 #include "./YDbDr/YDbDr.hpp"
+#endif
+
+#ifndef COLOR_EXCLUDE_MODEL_YPbPr
+#include "./YPbPr/YPbPr.hpp"
+#endif
+
 
 #include "./generic/operation/operation.hpp"
 #include "./generic/compare/compare.hpp"
