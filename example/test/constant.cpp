15785f409191e6850af5adc0e1730efb50052c50
dmilos <dmilos@example.com>
2016-11-13 12:16:31 +0100

    * test:[internal] test for constants

---
+++ example/test/constant.cpp
@@ -0,0 +1,115 @@
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+#include "color/color.hpp"
+
+
+static std::string print_color( std::string const& text,  ::color::rgb<std::uint8_t> const& r )
+ {
+  std::stringstream ss;
+
+  ss <<  "<div style=\"background-color:rgb("<< (unsigned)r[0] <<","<< (unsigned)r[1] <<","<< (unsigned)r[2] <<");color:white\">";
+
+  if( 0 != text.size() )
+   {
+    ss << text << " - ";
+   }
+  ss << "("<< (unsigned)r[0] <<","<< (unsigned)r[1] <<","<< (unsigned)r[2] <<")";
+
+  ss << "</div>";
+  return ss.str();
+ } 
+
+template< typename tag_name >
+ std::string print( ::color::constant::base< tag_name > const& constant, std::string const& name )
+ {
+  std::stringstream ss;
+  typedef ::color::constant::base< tag_name > constant_t;
+
+   ss << "<tr>" << std::endl;
+     ss << "<td  style=\"background-color:" << name << "\"> W3  -" << name << "</td>";
+     ss << "<td>" << print_color( "gray-"   + name, ::color::rgb<std::uint8_t>( ::color::gray<  double >( constant )  ) ) << "</td>";
+     ss << "<td>" << print_color( "rgb -"   + name, ::color::rgb<std::uint8_t>( ::color::rgb<   double >( constant )  ) ) << "</td>";
+     ss << "<td>" << print_color( "cmy -"   + name, ::color::rgb<std::uint8_t>( ::color::cmy<   double >( constant )  ) ) << "</td>";
+     ss << "<td>" << print_color( "cmyk-"   + name, ::color::rgb<std::uint8_t>( ::color::cmyk<  double >( constant )  ) ) << "</td>";
+     ss << "<td>" << print_color( "hsl -"   + name, ::color::rgb<std::uint8_t>( ::color::hsl<   double >( constant )  ) ) << "</td>";
+     ss << "<td>" << print_color( "hsv -"   + name, ::color::rgb<std::uint8_t>( ::color::hsv<   double >( constant )  ) ) << "</td>";
+     ss << "<td>" << print_color( "hsi -"   + name, ::color::rgb<std::uint8_t>( ::color::hsi<   double >( constant )  ) ) << "</td>";
+     ss << "<td>" << print_color( "yiq -"   + name, ::color::rgb<std::uint8_t>( ::color::yiq<   double >( constant )  ) ) << "</td>";
+     ss << "<td>" << print_color( "yuv -"   + name, ::color::rgb<std::uint8_t>( ::color::yuv<   double >( constant )  ) ) << "</td>";
+     ss << "<td>" << print_color( "YCgCo -" + name, ::color::rgb<std::uint8_t>( ::color::YCgCo< double >( constant )  ) ) << "</td>";
+     ss << "<td>" << print_color( "YDbDr -" + name, ::color::rgb<std::uint8_t>( ::color::YDbDr< double >( constant )  ) ) << "</td>";
+     ss << "<td>" << print_color( "YPbPr -" + name, ::color::rgb<std::uint8_t>( ::color::YPbPr< double >( constant )  ) ) << "</td>";
+    ss << "</tr>" << std::endl;
+
+  return ss.str();
+ }
+
+void test_constant()
+ {
+  std::stringstream ss; 
+
+  ss << "<table>";
+
+  ss << print( ::color::constant::aqua_t{},         "aqua"       );
+  ss << print( ::color::constant::aquamarine_t{},   "aquamarine" );
+  ss << print( ::color::constant::azure_t{},        "azure"      );
+  ss << print( ::color::constant::beige_t{},        "beige"      );
+  ss << print( ::color::constant::bisque_t{},       "bisque"     );
+  ss << print( ::color::constant::black_t{},        "black"      );
+  ss << print( ::color::constant::blue_t{},         "blue"       );
+  ss << print( ::color::constant::brown_t{},        "brown"      );
+  ss << print( ::color::constant::chocolate_t{},    "chocolate"  );
+  ss << print( ::color::constant::coral_t{},        "coral"      );
+  ss << print( ::color::constant::crimson_t{},      "crimson"    );
+  ss << print( ::color::constant::cyan_t{},         "cyan"       );
+  ss << print( ::color::constant::fuchsia_t{},      "fuchsia"    );
+  ss << print( ::color::constant::gainsboro_t{},    "gainsboro"  );
+  ss << print( ::color::constant::gold_t{},         "gold"       );
+  ss << print( ::color::constant::green_t{},        "green"      );
+  ss << print( ::color::constant::indigo_t{},       "indigo"     );
+  ss << print( ::color::constant::ivory_t{},        "ivory"      );
+  ss << print( ::color::constant::khaki_t{},        "khaki"      );
+  ss << print( ::color::constant::lavender_t{},     "lavender"   );
+  ss << print( ::color::constant::lime_t{},         "lime"       );
+  ss << print( ::color::constant::linen_t{},        "linen"      );
+  ss << print( ::color::constant::magenta_t{},      "magenta"    );
+  ss << print( ::color::constant::maroon_t{},       "maroon"     );
+  ss << print( ::color::constant::moccasin_t{},     "moccasin"   );
+  ss << print( ::color::constant::navy_t{},         "navy"       );
+  ss << print( ::color::constant::olive_t{},        "olive"      );
+  ss << print( ::color::constant::orange_t{},       "orange"     );
+  ss << print( ::color::constant::orchid_t{},       "orchid"     );
+  ss << print( ::color::constant::peru_t{},         "peru"       );
+  ss << print( ::color::constant::pink_t{},         "pink"       );
+  ss << print( ::color::constant::plum_t{},         "plum"       );
+  ss << print( ::color::constant::purple_t{},       "purple"     );
+  ss << print( ::color::constant::red_t{},          "red"        );
+  ss << print( ::color::constant::salmon_t{},       "salmon"     );
+  ss << print( ::color::constant::sienna_t{},       "sienna"     );
+  ss << print( ::color::constant::silver_t{},       "silver"     );
+  ss << print( ::color::constant::snow_t{},         "snow"       );
+  ss << print( ::color::constant::tan_t{},          "tan"        );
+  ss << print( ::color::constant::teal_t{},         "teal"       );
+  ss << print( ::color::constant::thistle_t{},      "thistle"    );
+  ss << print( ::color::constant::tomato_t{},       "tomato"     );
+  ss << print( ::color::constant::turquoise_t{},    "turquoise"  );
+  ss << print( ::color::constant::violet_t{},       "violet"     );
+  ss << print( ::color::constant::wheat_t{},        "wheat"      );
+  ss << print( ::color::constant::white_t{},        "white"      );
+  ss << print( ::color::constant::yellow_t{},       "yellow"     );
+
+
+  ss << "</table>";
+
+   {
+    std::ofstream ofs( "./constant.html" );
+    ofs <<  ss.str();
+   }
+
+
+ }
+
